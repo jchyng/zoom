a29dfe93baef74f0c52f00f34334b6a0fbb3703d
정찬영 <jcy0342@gmail.com>
2025-03-06 15:34:47 +0900

    init project

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+const hadleListen = () => {
+  console.log(`Server is running on port ${PORT}`);
+}
+app.listen(PORT, hadleListen);
